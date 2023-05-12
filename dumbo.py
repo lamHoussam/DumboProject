649d43bb26dfd3458a80d569bf2a5336bb346609
houssam lamlih <houssam.lamlih@hotmail.com>
2023-05-12 16:05:07 +0200

    Update dumbo grammar + simple parse

--- dumbo.py
+++ dumbo.py
@@ -1,8 +1,5 @@
-from lark import Lark
+from lark import Lark, Transformer, Tree
 import argparse
-
-
-
 
 def main():
     arg_parser = argparse.ArgumentParser(description='Dumbo programming language')
@@ -12,8 +9,25 @@ def main():
 
     args = arg_parser.parse_args()
 
+    data_file = args.data_file
+    template_file = args.template_file
+
     print(f'Data file: {args.data_file}')
-    print(f'Template file: {args.template_file}')
+    print(f'Template file: {template_file}')
+
+    with open('grammar.lark', 'r') as f:
+        grammar = f.read()
+
+    parser = Lark(grammar, start='programme')
+
+    with open(template_file, 'r') as f:
+        template_file_content = f.read()
+
+    output = parser.parse(template_file_content)
+    with open("output.txt", 'w') as f:
+        f.write(str(output))
+
+    print(output)
 
 
 if __name__ == '__main__':
